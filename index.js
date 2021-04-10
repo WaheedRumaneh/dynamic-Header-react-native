051eb36472575f70e6d8383b48b9d961afed3d1a
WaheedRumaneh <waheed.rumaneh@toreed.com>
2021-04-10 18:21:02 +0300

    last commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /**
  * @format
  */
-
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
